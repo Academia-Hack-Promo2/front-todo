026c89d428d4d53da0b071325d60ea5d73ba83d3
Jonathan Martinez <jamm@jmartinezm.org>
2015-05-11 12:57:12 -0430

    Creacion y listado de categorias, agreagado un margin-bottom entre tareas

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,68 @@
 $(function () {
-    $('#datetimepicker1').datetimepicker();
+  $('#datetimepicker1').datetimepicker();
+  var newCategory = $('#new_category');
+  var categories = $('#categories');
+  newCategory.hide();
+
+  categories.change(function(){
+    category = $(this).val();
+    if (category === 'Nueva'){
+      $('#new_category').show();
+    } else {
+      $('#new_category').hide();
+    }
+  });
+
+  $('#new_category').keyup(function(event){
+    if (event.which == 13){
+      $('#error_category').html('');
+      if ($('#new_category').val().length < 5){
+        $('#error_category').html('La categoria debe tener más de 5 caracteres');
+        return;
+
+      } else {
+
+        var category = {"title": newCategory.val()};
+          $.ajax({
+            type:'post',
+            url: 'http://localhost:4000/categories',
+            data: category,
+
+            success: function(data){
+              console.log(data);
+              $('#categories').prepend('<option value="'+data.id+'" selected>'+data.title+'</option>');
+              newCategory.val('');
+              newCategory.hide();
+            },
+
+            error: function(data){
+              console.log(data);
+            }
+
+          });
+      }
+
+    }
+  });
+
+  (function(){
+    $.ajax({
+      type:'get',
+      url: 'http://localhost:4000/categories',
+
+      success: function(data){
+        for (var i = 0; i < data.length; i++ ){
+          var categoryTitle = data[i].title;
+          $('#categories').prepend('<option>'+categoryTitle+'</option>');
+        }
+      },
+
+        error: function(data){
+          console.log(data);
+        }
+     
+    });
+  }());
+
+
 });
