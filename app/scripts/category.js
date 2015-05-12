6a45d2c77a2a897b1d13c4288a62156ce0819243
samuel melgarejo <samuel_021093@hotmail.com>
2015-05-12 16:00:32 -0430

    reducido category.js

--- app/scripts/category.js
+++ app/scripts/category.js
@@ -4,7 +4,6 @@
   $.ajax({
     type:'get',
     url: 'http://localhost:3000/categories',
-
     success: function(data){
       for (var i = 0; i < data.length; i++ ){          
         $('#category').append('<div class="col-md-4 col-sm-6 col-xs-12"><div class="box btn-info" data-category="' + data[i].id+ '"><br><br><h3>'+data[i].title+'</h3></div></div>');
@@ -14,7 +13,6 @@
         $.ajax({
           type:'get',
           url:'http://localhost:3000/tasks?category='+category_id,
-
           success: function(response){
             $('#tasks').html('     ');
             for(var i = 0; i < response.length; i++){
