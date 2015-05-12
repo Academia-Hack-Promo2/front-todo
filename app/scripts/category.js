848cc82ca8802274f138042a27c7b7b9fb632d5b
samuel melgarejo <samuel_021093@hotmail.com>
2015-05-12 15:54:25 -0430

    modifica category.html , js , css

--- app/scripts/category.js
+++ app/scripts/category.js
@@ -1,12 +1,13 @@
 // reutilizar maqueta y listar categorias Alex Ballera
 (function(){
+  $('#category').html('<div class="btn-primary title"><h2>categorias</h2></div>')
   $.ajax({
     type:'get',
     url: 'http://localhost:3000/categories',
 
     success: function(data){
       for (var i = 0; i < data.length; i++ ){          
-        $('#category').append('<div class="col-md-4 col-sm-6 col-xs-12"><div class="box btn-info" data-category="' + data[i].id+ '"><h3>'+data[i].title+'</h3></div></div>');
+        $('#category').append('<div class="col-md-4 col-sm-6 col-xs-12"><div class="box btn-info" data-category="' + data[i].id+ '"><br><br><h3>'+data[i].title+'</h3></div></div>');
         console.log(data);
       }
       $(".box").click(function(){
@@ -16,7 +17,7 @@
           url:'http://localhost:3000/tasks?category='+category_id,
 
           success: function(response){
-            $('#tasks').html('     ')
+            $('#tasks').html('     ');
             for(var i = 0; i < response.length; i++){
               $('#tasks').append('<div class="col-md-4 col-sm-6 col-xs-12"><div class="box btn-warning"><h3>titulo:  "'+ response[i].title+'"</h3><h3r><h3>Fecha:  "'+ response[i].finish_date+'"</h3><h3r><h3>Tipo:  "'+ response[i].task_type+'"</h3><h3>Estado:  "'+ response[i].status+'"</h3></div></div>')
             } 
